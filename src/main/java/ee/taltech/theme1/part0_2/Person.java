7b05f25b17d888bf9674a181b9796ae993b55644
Oleg Pahhomov <oleg.pahhomov@gmail.com>
2021-01-23 19:29:15 +0200

    init project

---
+++ src/main/java/ee/taltech/theme1/part0_2/Person.java
@@ -0,0 +1,30 @@
+package ee.taltech.theme1.part0_2;
+
+public class Person {
+
+    public static void main(String[] args) {
+        Person bob = new Person();
+        bob.setName("Bob");
+        System.out.println(bob.getName());
+
+        Person mary = new Person("Mary");
+        System.out.println(mary.getName());
+    }
+
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
