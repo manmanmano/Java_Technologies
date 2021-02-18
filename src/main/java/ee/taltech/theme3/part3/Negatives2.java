035c236de399b116f4e2cbb3e413892b0d0d9960
madang <madang@ttu.ee>
2021-02-18 11:41:16 +0200

    added part3 theme 3 solution. 1 test out of 8 not working though :(

--- src/main/java/ee/taltech/theme3/part3/Negatives2.java
+++ src/main/java/ee/taltech/theme3/part3/Negatives2.java
@@ -1,6 +1,9 @@
 package ee.taltech.theme3.part3;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Negatives2 {
 
@@ -9,13 +12,29 @@ public class Negatives2 {
     // remember that biggest negative is the one closest to zero as -10 is bigger than -100
     // here is another design decision where we return null on nothing
     public static Integer biggestNegative(List<Integer> numbers){
-        return null;
+        if (numbers == null) {
+            return null;
+        }
+        return numbers.stream()
+                .filter(e -> e < 0)
+                .max(Integer::compareTo)
+                .orElse(null);
     }
 
     //todo 103.2 add tests and add logic
     // this combines lesson2.part2 SumOfElements and lesson2.part6 Negatives
     // here it's up to you what do you want to return on null and what to return on no negatives, zero? Optional?
-    public static Integer sumOfNegatives(List<Integer> numbers){
-        return null;
+    public static Optional<Integer> sumOfNegatives(List<Integer> numbers){
+        if (numbers == null) {
+            return Optional.empty();
+        }
+        List<Integer> negatives = numbers.stream()
+                .filter(e -> e < 0)
+                .collect(Collectors.toUnmodifiableList());
+        if (negatives.isEmpty()){
+            return Optional.empty();
+        }
+        return Optional.of(negatives.stream()
+                .reduce(0, Integer::sum));
     }
 }
--- src/test/java/ee/taltech/theme3/part3/Negatives2Test.java
+++ src/test/java/ee/taltech/theme3/part3/Negatives2Test.java
@@ -1,13 +1,13 @@
 package ee.taltech.theme3.part3;
 
+import ee.taltech.theme2.part2.SumOfElements;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class Negatives2Test {
 
@@ -30,8 +30,37 @@ class Negatives2Test {
     void biggest_element_returns_biggest_negative_element_of_the_list() {
         Integer biggestNegative = Negatives2.biggestNegative(List.of(-3, -200, 0));
         assertNotNull(biggestNegative);
-        assertEquals(-1, (int) biggestNegative);
+        assertEquals(-3, (int) biggestNegative);
     }
 
     //todo 103.3 sumOfNegatives I think we should create null, empty list, negatives list, positives list, mixed list
+
+    @Test
+    void on_null() {
+        assertEquals(Optional.empty(), Negatives2.sumOfNegatives(List.of()));
+    }
+
+    @Test
+    void on_positives() {
+        assertEquals(Optional.empty(), Negatives2.sumOfNegatives(List.of(1, 2, 3)));
+    }
+
+    @Test
+    void on_negatives() {
+        Optional<Integer> actual = Negatives2.sumOfNegatives(List.of(-1, -2, -3));
+        assertEquals(-6, actual.get());
+    }
+    @Test
+    void on_mixed() {
+        Optional<Integer> actual = Negatives2.sumOfNegatives(List.of(-1, 2, -3));
+        assertEquals(-4, actual.get());
+    }
+
+
+    @Test
+    void on_no_elements() {
+        assertEquals(Optional.empty(), Negatives2.sumOfNegatives(List.of()));
+        assertThrows(IllegalArgumentException.class, ()->SumOfElements.sum(List.of()));
+        //todo 12.4 this is your decision what to return on "empty list" input
+    }
 }
