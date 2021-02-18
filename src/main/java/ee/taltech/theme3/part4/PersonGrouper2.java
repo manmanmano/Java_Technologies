335bcbdd029a92d0def4283e16e3183c875f07c7
madang <madang@ttu.ee>
2021-02-18 11:53:52 +0200

    finish

--- src/main/java/ee/taltech/theme3/part4/PersonGrouper2.java
+++ src/main/java/ee/taltech/theme3/part4/PersonGrouper2.java
@@ -1,8 +1,10 @@
 package ee.taltech.theme3.part4;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PersonGrouper2 {
 
@@ -10,22 +12,28 @@ public class PersonGrouper2 {
     // this draws heavily from lesson2.part10
     // fix tests and add logic for these two methods, however use streams now
     public static Map<String, Person2> mapPeopleByIdCode(List<Person2> people) {
-        return new HashMap<>();
+        // same as: people.stream().collect.Collectors.toMap(person -> person.getIdCode(), person -> person));
+        return people.stream().collect(Collectors.toMap(Person2::getIdCode, person -> person));
     }
 
     public static Map<String, Person2> mapPeopleByFirstAndLastName(List<Person2> people) {
-        return new HashMap<>();
+
+        return people.stream().collect(Collectors.toMap(person -> fullname(person), person -> person));
+    }
+
+    private static String fullname(Person2 person) {
+        return person.getFirstName() + " " + person.getLastName();
     }
 
     //todo 104.2 can you make this work as well?
     // ... is a varargs argument
     public static Map<String, Person2> mapPeopleByIdCode(Person2... people) {
-        return new HashMap<>();
+        return Arrays.stream(people).collect(Collectors.toMap(Person2::getIdCode, person -> person));
     }
 
     //todo 104.3 while lesson2.part10 was hardcore, this one should be quite easy
     // you need to group on the stream
     public static Map<String, List<Person2>> groupPeopleByFirstAndLastNameMultiple(List<Person2> people) {
-        return new HashMap<>();
+        return people.stream().collect(Collectors.groupingBy(PersonGrouper2::fullname));
     }
 }
--- src/test/java/ee/taltech/theme3/part4/PersonGrouper2Test.java
+++ src/test/java/ee/taltech/theme3/part4/PersonGrouper2Test.java
@@ -14,6 +14,11 @@ class PersonGrouper2Test {
             new Person2("11111", "Great", "President"),
             new Person2("22222", "Donald", "Duck"),
             new Person2("33333", "Cristiano", "Ronaldo"));
+    static List<Person2> testPeople2 = List.of(
+            new Person2("11111", "Great", "President"),
+            new Person2("00000", "Great", "President"),
+            new Person2("22222", "Donald", "Duck"),
+            new Person2("33333", "Cristiano", "Ronaldo"));
 
     @Test
     void grouping_people_by_id_code_divides_them_to_id_code_and_person_pairs() {
@@ -38,6 +43,37 @@ class PersonGrouper2Test {
         Person2 person2 = map.get("Great President");
         assertEquals("Great", person2.getFirstName());
         Person2 person3 = map.get("Cristiano Ronaldo");
+        assertEquals("Cristiano", person3.getFirstName());
+    }
+    @Test
+    void grouping_people_by_id_code_divides_them_to_id_code_and_person_pairs_varargs() {
+        Map<String, Person2> map = PersonGrouper2.mapPeopleByIdCode(
+                new Person2("11111", "Great", "President"),
+                new Person2("22222", "Donald", "Duck"),
+                new Person2("33333", "Cristiano", "Ronaldo"));
+        assertEquals(3, map.size());
+        assertNull(map.get("-1"));
+        Person2 person1 = map.get("11111");
+        assertEquals("Great", person1.getFirstName());
+        Person2 person2 = map.get("22222");
+        assertEquals("Donald", person2.getFirstName());
+        Person2 person3 = map.get("33333");
+        assertEquals("Cristiano", person3.getFirstName());
+    }
+    @Test
+    void grouping_people_by_first_and_last_name_code_divides_them_to_full_name_and_person_pairs_with_list() {
+        Map<String, List<Person2>> map = PersonGrouper2.groupPeopleByFirstAndLastNameMultiple(testPeople2);
+        assertEquals(3, map.size());
+        assertNull(map.get("-1"));
+        Person2 person1 = map.get("Donald Duck").get(0);
+        assertEquals("Donald", person1.getFirstName());
+        List<Person2> person2 = map.get("Great President");
+        assertEquals(2, person2.size());
+        Person2 person2First = map.get("Great President").get(0);
+        assertEquals("Great", person2First.getFirstName());
+        Person2 person2Second = map.get("Great President").get(1);
+        assertEquals("Great", person2Second.getFirstName());
+        Person2 person3 = map.get("Cristiano Ronaldo").get(0);
         assertEquals("Cristiano", person3.getFirstName());
     }
 }
